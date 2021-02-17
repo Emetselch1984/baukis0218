9ce4330c00b6d8456d3014d0a2d7e9a766dae4d7
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-02-18 08:05:48 +0900

    Add scss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  layout :set_out
+
+  private def set_out
+    if params[:controller].match(%r{\A(staff|admin|customer)/})
+      Regexp.last_match[1]
+    else
+      "customer"
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def document_title
+    @title.present? ? "#{@title} -BAUKIS2021" : "BAUKIS2021"
+  end
 end
